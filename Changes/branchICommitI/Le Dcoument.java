dd6dd2c97be28aba0468a501a54b3caf1e656979
raaveinm <dulovkir72@gmail.com>
2025-02-27 09:14:12 +0300

    Update Le Dcoument.java
    third commit

--- Changes/branchICommitI/Le Dcoument.java
+++ Changes/branchICommitI/Le Dcoument.java
@@ -1,5 +1,10 @@
+import LeDocument2;
+
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        String message = "Message";
+        LeDocument2.Printer(message);
     }
 }
